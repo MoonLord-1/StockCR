84152e8dbe34811f15fa60caee1e3c5b7cee7cb7
MoonLord-1 <paulovargas052@gmail.com>
2026-08-08 15:13:39 -0600

    fea: create entities, category,merchant and StockMovement, but needed some fixes

--- src/main/java/com/yeirel/StockCR/entities/category.java
+++ src/main/java/com/yeirel/StockCR/entities/category.java
@@ -1,4 +1,19 @@
 package com.yeirel.StockCR.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "categories")
 public class category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
 }
